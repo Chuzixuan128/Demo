10ea0876747cede9d55be232b63c6532973194b7
chuzixuan@qq.com <chuzixuan@qq.com>
2019-05-29 17:12:01 +0800

    finxaisi

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.lenovo.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.lenovo.myapplication.R;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private Button mBnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBnt = (Button) findViewById(R.id.bnt);
+        mBnt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.bnt:
+                break;
+        }
     }
 }
