a5dae3e50a58f68f58ede66b6583425880b90bbb
chuzixuan@qq.com <chuzixuan@qq.com>
2019-05-29 17:20:26 +0800

    uigvbvk

--- app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mBnt;
+    /**
+     * Hello World!
+     */
+    private TextView mIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         mBnt = (Button) findViewById(R.id.bnt);
         mBnt.setOnClickListener(this);
+        mIt = (TextView) findViewById(R.id.it);
     }
 
     @Override
@@ -27,7 +33,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
             case R.id.bnt:
+                initdata();
                 break;
         }
+    }
+
+    private void initdata() {
+            mIt.setText("阿萨德加");
     }
 }
